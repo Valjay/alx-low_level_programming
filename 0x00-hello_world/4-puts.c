f33b00c1e9e68de0e6ea43837d64eb10b0cfab0c
Valjay <jj350055@gmail.com>
2022-09-09 03:56:13 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
-
- /**
-  * main - Print a text
-  *
-  * Return: Always zero (Success)
-  */
+/**
+ * main - main block
+ * Return: 0
+ */
 int main(void)
 {
-
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
