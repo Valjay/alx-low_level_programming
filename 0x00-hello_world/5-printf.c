62e8a382654363bae0f3ba0146f0fdec85d687ee
Valjay <jj350055@gmail.com>
2022-09-08 22:59:12 -0700

    c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
-#include <stdio.h>
-/**
- * main -main block
- * Return: always 0
-*/
-int main(void)
-{
+ #include <stdio.h>
+ /**
+  * main -main block
+  * Return: always 0
+  */
+ int main(void)
+ {
  printf("with proper grammar, but the outcome is a piece of art, \n");
  return (0);
-}
+ }
