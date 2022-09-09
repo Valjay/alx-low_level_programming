57459a7bcff4d07b41acaba27fd7ee3a68384a56
Valjay <jj350055@gmail.com>
2022-09-09 02:56:10 -0700

    c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h
+#include <unistd.h>
+/**
+main Entry point
+Return:l
+*/
+int main(void)
+{
+write(2,
+"and that piece of art is useful\" Dora Koroar, 2015-10-19\n", 591)
+return (1):
+}
