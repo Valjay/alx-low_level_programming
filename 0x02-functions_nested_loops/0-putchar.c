94b004e6e0ed4cf5c2f3a2dbb4bf620b9449a388
Valjay <jj350055@gmail.com>
2022-09-13 09:50:34 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,8 @@
 #include "main.h"
-
 /**
  * main - print putchar
  * Return: always 0
  */
-
 int main(void)
 {
 	_putchar('_');
@@ -16,7 +14,5 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-
-	return (0);
-
+return (0);
 }
