221a663bafe0a55f8e39cbc77728e8d7c321b557
Valjay <jj350055@gmail.com>
2022-09-13 09:41:52 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,22 @@
-#include <stdio.h>
-/*-putchar-writes character c to stdout
-*@c-the character to print
-*
-*return : on success 0
-*on error, 1 is returned
-*/
-int -putchar(char c)
-{
-return 0;
-}
+#include "main.h"
 
+/**
+ * main - print putchar
+ * Return: always 0
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+
+}
