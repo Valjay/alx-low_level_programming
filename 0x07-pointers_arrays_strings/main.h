982a6d8586d2a0c7359d845715645eebbaf5e246
Valjay <jj350055@gmail.com>
2022-09-26 20:41:45 -0700

    pointers and pointing

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,10 +1,10 @@
-ifndef MAIN_H
+#ifndef MAIN_H
 #define MAIN_H
-int _putchar(char c);
 /**
- *void prototypes()
- *int prototypes()
- */
+*void prototypes()
+*int prototypes()
+*/
+int _putchar(char);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
