3ddaeebae2c5d3fe750a9137238291f283878cf8
Valjay <jj350055@gmail.com>
2022-10-20 18:41:45 -0700

    singly linked list

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,13 +1,40 @@
 #include "lists.h"
-void exec_before_main(void)__attribute__((constructor));
-
 /**
- * exec_before_main - function executes before main
- *
- * Return: nothing
+ * add_node - adds node to the beginning of a linked list
+ * @head: first node of the list to be substituted
+ * @str: string to be duplicated
+ * Return: number of elements in a list
  */
-void exec_before_main(void)
+list_t *add_node(list_t **head, const char *str)
 {
-	printf("You're beat! and yet, you must allow,\n");
-	printf("I bore my house upon my back!\n");
+	unsigned int str_len;
+	list_t *new_node;
+
+	if (str == NULL)
+		return (NULL);
+
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	/* compute length of str */
+	for (str_len = 0; str[str_len] != '\0'; str_len++)
+		;
+
+	/* put in data */
+	new_node->len = str_len;
+	new_node->str = strdup(str);
+
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	/* point the new node to the current value of head */
+	new_node->next = (*head);
+	/**
+	 * move head to point to new node
+	 */
+	(*head) = new_node;
+	return (new_node);
 }
